2171408f88a43ff1839103389a17275b4bf922a9
fleshgoblin <44947785+fleshgoblin@users.noreply.github.com>
2018-11-11 20:12:26 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setStatus('Online')
-  client.user.setActivity("Napping (since 11:49am)",{type: "PLAYING"});
+  client.user.setActivity
 });
 
 client.on('message', msg => {
